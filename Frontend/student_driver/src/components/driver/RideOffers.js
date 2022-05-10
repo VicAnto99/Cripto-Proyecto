ec004d2b3012d6d7d1ec3061368fa4179d5ee052
Monica Lara <38739285+itsmonicalara@users.noreply.github.com>
2022-05-10 00:41:37 -0500

    Empty data
    Co-Authored-By: Vock99 <15371718+VicAnto99@users.noreply.github.com>
    Co-Authored-By: Raúl González Cardona <44035290+Raul5412@users.noreply.github.com>

--- Frontend/student_driver/src/components/driver/RideOffers.js
+++ Frontend/student_driver/src/components/driver/RideOffers.js
@@ -1,12 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useAuth0 } from "@auth0/auth0-react";
 import './RideOffers.css'
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 function RideOffers() {
 
     const { loginWithRedirect } = useAuth0();
+
+
+    // Test
+    let navigate = useNavigate();
+
+    const routeChange = () =>{ 
+        navigate(-1);
+    }
+
+    const [data, setData] = useState({
+        from: '',
+        to: '',
+        date: '',
+        positions: '',
+        cost: '',
+        comments: '',
+    });
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        setData({
+          ...data,
+          [e.target.name]: value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(data);
+        const userData = {
+            from: data.from,
+            to: data.to,
+            date: data.date,
+            positions: data.positions,
+            cost: data.cost,
+            comments: data.comments,
+        };
+        axios.post('http://localhost:4000/student_drive/add_offer', userData).then((response) => {
+            console.log(response.data);
+            console.log(response.data.token);
+            routeChange();
+        });   
+    };
    
-     return (
+    return (
         <div className='container'>
         <div className="card">
             <hr></hr>
@@ -18,15 +63,15 @@ function RideOffers() {
                         <div className="card-body">
                             <h5 className="card-title">Rideshare from:</h5>
                             <form className="form-floating">
-                                <input type="time" class="form-control" id="floatingPassword"/>
+                                <input type="time" class="form-control" id="floatingPassword" name='from' value={data.from} onChange={handleChange}/>
                             </form>
                             <h5 className="card-title">To:</h5>
                             <form className="form-floating">
-                                <input type="time" class="form-control" id="floatingPassword"/>
+                                <input type="time" class="form-control" id="floatingPassword" name='to' value={data.to} onChange={handleChange}/>
                             </form>
                             <h5 className="card-title">Date:</h5>
                             <form className="form-floating">
-                                <input type="date" class="form-control" id="floatingPassword"/>
+                                <input type="date" class="form-control" id="floatingPassword" name='date' value={data.date} onChange={handleChange}/>
                             </form>
                         </div>
                     </div>
@@ -36,22 +81,22 @@ function RideOffers() {
                         <div className="card-body">
                             <h5 className="card-title">Positions:</h5>
                             <form className="form-floating">
-                                <input type="number" class="form-control" id="floatingPassword"/>
+                                <input type="number" class="form-control" id="floatingPassword" name='positions' value={data.positions} onChange={handleChange}/>
                             </form>
                             <h5 className="card-title">Cost:</h5>
                             <form className="form-floating">
-                                <input type="number" class="form-control" id="floatingPassword" placeholder='0.00 MXN'/>
+                                <input type="number" class="form-control" id="floatingPassword" placeholder='0.00 MXN' name='cost' value={data.cost} onChange={handleChange}/>
                             </form>
                             <h5 className="card-title">Additional Comments:</h5>
                             <form className="form-floating">
-                                <input input='text' class="form-control" id="floatingPassword" placeholder='0.00 MXN'/>
+                                <input input='text' class="form-control" id="floatingPassword" placeholder='0.00 MXN' name='comments' value={data.comments} onChange={handleChange}/>
                             </form>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <button type="button" className="btn btn-success">Offer</button>
+         <button type="submit" className="btn btn-success" onClick={handleSubmit}>Offer</button>
 
     </div>
    )
