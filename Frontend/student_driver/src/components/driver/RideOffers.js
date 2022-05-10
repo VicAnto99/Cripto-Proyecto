3d299e3b6ba35a931aaa6c3e85186a9774487d9c
Monica Lara <38739285+itsmonicalara@users.noreply.github.com>
2022-05-10 15:17:25 -0500

    Fixed new bid

--- Frontend/student_driver/src/components/driver/RideOffers.js
+++ Frontend/student_driver/src/components/driver/RideOffers.js
@@ -53,51 +53,53 @@ function RideOffers() {
    
     return (
         <div className='container'>
-        <div className="card">
-            <hr></hr>
-            <h4 className="card-title text-center"> New Rideshare Publication </h4>
-            <hr></hr>
-            <div className="row">
-                <div className="col-sm-6">
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Rideshare from:</h5>
-                            <form className="form-floating">
-                                <input type="time" class="form-control" id="floatingPassword" name='from' value={data.from} onChange={handleChange}/>
-                            </form>
-                            <h5 className="card-title">To:</h5>
-                            <form className="form-floating">
-                                <input type="time" class="form-control" id="floatingPassword" name='to' value={data.to} onChange={handleChange}/>
-                            </form>
-                            <h5 className="card-title">Date:</h5>
-                            <form className="form-floating">
-                                <input type="date" class="form-control" id="floatingPassword" name='date' value={data.date} onChange={handleChange}/>
-                            </form>
+            <div className="card">
+                <hr></hr>
+                <h4 className="card-title text-center"> New Rideshare Publication </h4>
+                <hr></hr>
+                <div className="row">
+
+                    <div className="col-sm-6">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Rideshare from:</h5>
+                                <form className="form-floating">
+                                    <input type="time" class="form-control" id="floatingPassword" name='from' value={data.from} onChange={handleChange}/>
+                                </form>
+                                <h5 className="card-title">To:</h5>
+                                <form className="form-floating">
+                                    <input type="time" class="form-control" id="floatingPassword" name='to' value={data.to} onChange={handleChange}/>
+                                </form>
+                                <h5 className="card-title">Date:</h5>
+                                <form className="form-floating">
+                                    <input type="date" class="form-control" id="floatingPassword" name='date' value={data.date} onChange={handleChange}/>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="col-sm-6">
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Positions:</h5>
-                            <form className="form-floating">
-                                <input type="number" class="form-control" id="floatingPassword" name='positions' value={data.positions} onChange={handleChange}/>
-                            </form>
-                            <h5 className="card-title">Cost:</h5>
-                            <form className="form-floating">
-                                <input type="number" class="form-control" id="floatingPassword" placeholder='0.00 MXN' name='cost' value={data.cost} onChange={handleChange}/>
-                            </form>
-                            <h5 className="card-title">Additional Comments:</h5>
-                            <form className="form-floating">
-                                <input input='text' class="form-control" id="floatingPassword" placeholder='0.00 MXN' name='comments' value={data.comments} onChange={handleChange}/>
-                            </form>
+
+                    <div className="col-sm-6">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Positions:</h5>
+                                <form className="form-floating">
+                                    <input type="number" class="form-control" id="floatingPassword" name='positions' value={data.positions} onChange={handleChange}/>
+                                </form>
+                                <h5 className="card-title">Cost:</h5>
+                                <form className="form-floating">
+                                    <input type="number" class="form-control" id="floatingPassword" placeholder='0.00 MXN' name='cost' value={data.cost} onChange={handleChange}/>
+                                </form>
+                                <h5 className="card-title">Additional Comments:</h5>
+                                <form className="form-floating">
+                                    <input input='text' class="form-control" id="floatingPassword" placeholder='0.00 MXN' name='comments' value={data.comments} onChange={handleChange}/>
+                                </form>
+                            </div>
                         </div>
                     </div>
+                    
                 </div>
             </div>
-        </div>
-         <button type="submit" className="btn btn-success" onClick={handleSubmit}>Offer</button>
-
+            <button type="submit" className="btn btn-success" onClick={handleSubmit}>Offer</button>
     </div>
    )
  }
--- Frontend/student_driver/src/components/student/NewBid.js
+++ Frontend/student_driver/src/components/student/NewBid.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { useAuth0 } from "@auth0/auth0-react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function NewBid(){
 
@@ -9,49 +7,70 @@ function NewBid(){
         <div className='container text-center'>
             <div className="card">
                 <h3 className="card-title">New Bid</h3>
-                <div className="card">
-                    <div className="card-body">
-                        <h5 className="card-title">Rideshare details</h5>
-                        <div className="form-floating mb-3">
-                            <input type="text" className="form-control" id="floatingDriver" placeholder="Victor Godinez"/>
-                            <label for="floatingDriver">Driver</label>
-                        </div>
-                        <div className="form-floating mb-3">
-                            <input type="text" className="form-control" id="floatingPositionsOpen" placeholder="Victor Godinez"/>
-                            <label for="floatingPositionsOpen">Positions Open</label>
-                        </div>
-                        <div className="form-floating mb-3">
-                            <input type="text" className="form-control" id="floatingAdditionalComments" placeholder="Victor Godinez"/>
-                            <label for="floatingAdditionalComments">Additional comments</label>
-                        </div>
-                        <div className="form-floating mb-3">
-                            <input type="text" className="form-control" id="floatingPlace" placeholder="Victor Godinez"/>
-                            <label for="floatingPlace">Place</label>
-                        </div>
-                        <div className="form-floating mb-3">
-                            <input type="number" className="form-control" id="floatingCost" min="0.00" max="10000.00" step="0.01" placeholder="Victor Godinez"/>
-                            <label for="floatingCost">Cost</label>
-                        </div>
-                        <div className="col-sm-4">
-                            <div className="form-floating mb-3">
-                                <input type="time" className="form-control" id="floatingPeriod" placeholder="From 8:00 to 16:00"/>
-                                <label for="floatingPeriod">Period</label>
+                <div className="row">
+
+                    <div className="col-sm-6">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Rideshare details</h5>
+                                <div className="form-floating mb-3">
+                                    <input type="text" className="form-control" id="floatingDriver" placeholder="Victor Godinez"/>
+                                    <label for="floatingDriver">Driver</label>
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <input type="text" className="form-control" id="floatingPositionsOpen" placeholder="Victor Godinez"/>
+                                    <label for="floatingPositionsOpen">Positions Open</label>
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <input type="text" className="form-control" id="floatingAdditionalComments" placeholder="Victor Godinez"/>
+                                    <label for="floatingAdditionalComments">Additional comments</label>
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <input type="text" className="form-control" id="floatingPlace" placeholder="Victor Godinez"/>
+                                    <label for="floatingPlace">Place</label>
+                                </div>
+                                <div className="form-floating mb-3">
+                                    <input type="number" className="form-control" id="floatingCost" min="0.00" max="10000.00" step="0.01" placeholder="Victor Godinez"/>
+                                    <label for="floatingCost">Cost</label>
+                                </div>
+                                <div className="col-sm-4">
+                                    <div className="form-floating mb-3">
+                                        <input type="time" className="form-control" id="floatingPeriod" placeholder="From 8:00 to 16:00"/>
+                                        <label for="floatingPeriod">Period</label>
+                                    </div>
+                                </div>
+                                <div className="col-sm-4">
+                                    <h6>TO</h6>
+                                </div>
+                                <div className="col-sm-4">
+                                    <div className="form-floating mb-3">
+                                        <input type="time" className="form-control" id="floatingPeriod2" placeholder="From 8:00 to 16:00"/>
+                                        <label for="floatingPeriod2">Period</label>
+                                    </div>
+                                </div>                                      
                             </div>
                         </div>
-                        <div className="col-sm-4">
-                            <h6>TO</h6>
-                        </div>
-                        <div className="col-sm-4">
-                            <div className="form-floating mb-3">
-                                <input type="time" className="form-control" id="floatingPeriod2" placeholder="From 8:00 to 16:00"/>
-                                <label for="floatingPeriod2">Period</label>
-                            </div>
-                        </div>
-                        <button type="button" className="btn btn-success">Bid</button>
                     </div>
+
+                    <div className="col-sm-6">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Bid details</h5>
+                                <div className="form-floating mb-3">
+                                    <input type="number" className="form-control" id="floatingCost" min="0.00" max="10000.00" step="0.01" placeholder="Victor Godinez"/>
+                                    <label for="floatingCost">Amount</label>
+                                    <br></br>
+                                    <button type="button" className="btn btn-success">Bid</button>
+                                </div>   
+                            </div>
+                        </div>            
+                    </div>
+
                 </div>
             </div>
         </div>
+
+
     )
 }
 
