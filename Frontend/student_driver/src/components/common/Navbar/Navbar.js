3b24bc098aec07c1edcdd1be2c05c84d839af7e0
Monica Lara <38739285+itsmonicalara@users.noreply.github.com>
2022-05-09 23:05:52 -0500

    Express running

--- Frontend/student_driver/src/components/common/Navbar/Navbar.js
+++ Frontend/student_driver/src/components/common/Navbar/Navbar.js
@@ -1,27 +1,24 @@
 import React from 'react'
 import Logout from '../Logout.js';
+import { Nav, NavLink, Bars, NavMenu } from './NavbarElements';
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <div className="home_navbar">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="/">StudentDrive</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a class="nav-link active" aria-current="page" href="/">Home</a>
-              <a class="nav-link" href='/rideOffers'>RideShare Offers</a>
-              <a class="nav-link" href='/newBid'>New Bid</a>
-              <a class='nav-link' href='#'>About us</a>
-            </div>
-          </div>
-          <Logout />
-        </div>   
-      </nav>
-    </div>
+    <>
+      <Nav>
+        <NavLink to='/'>
+          <h2>StudentDrive</h2>
+        </NavLink>
+        <Bars/>
+          <NavMenu>
+            <NavLink to='/rideOffers' activeStyle>Rideshare Offers</NavLink>
+            <NavLink to='/newBid' activeStyle>New Bid</NavLink>
+            <NavLink to='/contact' activeStyle>Contact Us</NavLink>
+          </NavMenu>
+          <Logout/>
+      </Nav>
+    </>
   )
 }
 
