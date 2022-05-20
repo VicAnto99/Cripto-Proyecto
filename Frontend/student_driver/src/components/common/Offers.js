8a02298ec95d4259fab2769e7e22bca52b8620b2
Monica Lara <38739285+itsmonicalara@users.noreply.github.com>
2022-05-19 23:14:31 -0500

    Update Offers.js
    Co-Authored-By: OmarAlexDev <69879119+OmarAlexDev@users.noreply.github.com>
    Co-Authored-By: Vock99 <15371718+VicAnto99@users.noreply.github.com>
    Co-Authored-By: Raúl González Cardona <44035290+Raul5412@users.noreply.github.com>

--- Frontend/student_driver/src/components/common/Offers.js
+++ Frontend/student_driver/src/components/common/Offers.js
@@ -17,14 +17,59 @@ function Offers() {
             <tr>
               <th scope="col">User</th>
               <th scope="col">Date</th>
-              <th scope="col">Time</th>
+              <th scope="col"> Start Time</th>
+              <th scope="col"> End Time</th>
               <th scope="col">Positions</th>
-              <th scope="col">Place</th>
-              <th scope="col">Offer</th>
+              <th scope="col"> Start Place</th>
+              <th scope="col"> End Place</th>
+              <th scope="col">Duration</th>
+              <th scope="col">Cost</th>
+              <th scope="col">Offer - 100</th>
+              <th scope="col">Offer - 200</th>
+              <th scope="col">Offer - 300</th>
             </tr>
           </thead>
           <tbody>
-            
+            <tr>
+              <th scope="row">user_1</th>
+              <td>Monday 25 May</td>
+              <td>19:00</td>
+              <td>20:00</td>
+              <td>2</td>
+              <td>Ciudad Universitaria</td>
+              <td>Polanco</td>
+              <td>1 hour</td>
+              <td>10ETH</td>             
+              <td>
+                  <button type="button" class="btn btn-info btn-circle btn-lg"><i class="glyphicon glyphicon-ok"></i></button>
+              </td>
+              <td>
+                  <button type="button" class="btn btn-info btn-circle btn-lg"><i class="glyphicon glyphicon-ok"></i></button>
+              </td>
+              <td>
+                  <button type="button" class="btn btn-info btn-circle btn-lg"><i class="glyphicon glyphicon-ok"></i></button>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">user_1</th>
+              <td>Monday 25 May</td>
+              <td>13:00</td>
+              <td>14:30</td>
+              <td>1</td>
+              <td>Neza</td>
+              <td>Tlalpan</td>
+              <td>2 hour</td>
+              <td>12ETH</td>             
+              <td>
+                  <button type="button" class="btn btn-info btn-circle btn-lg"><i class="glyphicon glyphicon-ok"></i></button>
+              </td>
+              <td>
+                  <button type="button" class="btn btn-info btn-circle btn-lg"><i class="glyphicon glyphicon-ok"></i></button>
+              </td>
+              <td>
+                  <button type="button" class="btn btn-info btn-circle btn-lg"><i class="glyphicon glyphicon-ok"></i></button>
+              </td>
+            </tr>
           </tbody>
         </table>
         <div className='offers_container_title'>
