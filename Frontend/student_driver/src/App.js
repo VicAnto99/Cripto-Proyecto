a1fca875d2ae2aba8e6fd343e285bf9b46a8c816
Monica Lara <38739285+itsmonicalara@users.noreply.github.com>
2022-05-19 23:45:23 -0500

    Account
    Co-Authored-By: OmarAlexDev <69879119+OmarAlexDev@users.noreply.github.com>
    Co-Authored-By: Vock99 <15371718+VicAnto99@users.noreply.github.com>
    Co-Authored-By: Raúl González Cardona <44035290+Raul5412@users.noreply.github.com>

--- Frontend/student_driver/src/App.js
+++ Frontend/student_driver/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Web3 from 'web3';
 import './App.css';
 import { useAuth0 } from "@auth0/auth0-react";
 import Login from './components/common/Login.js';
@@ -8,6 +9,17 @@ import Home from './components/common/Home.js';
 function App() {
 
   const { isAuthenticated } = useAuth0();
+  const [account, setAccount] = useState(); // state variable to set account.
+
+  useEffect(() => {
+    async function load(){
+      const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
+      const accounts = await web3.eth.requestAccounts();
+
+      setAccount(accounts[0]);
+    }
+    load();
+  }, []);
 
   return (
     <div className='app'>
@@ -15,6 +27,7 @@ function App() {
       {isAuthenticated ? (
         <>
           <Home />
+          Your account is: {account}
         </>  
       ) : (
         <Login />
